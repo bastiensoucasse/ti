f9ea7fa5df3088dce6cb065fc8982095d1844a38
Bastien Soucasse <bastien.soucasse@icloud.com>
2022-01-26 12:12:40 +0100

    Missing last activity
    Co-authored-by: iprovost <iprovost@users.noreply.github.com>

--- td2/sed.c
+++ td2/sed.c
@@ -1,67 +1,69 @@
 #include <assert.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "sed.h"
 #include "colorimetric_data.h"
+#include "sed.h"
 
 #include "MISSINGCODE.h"
 
-float *
+float*
 sed_alloc(void)
 {
-    float *sed = malloc(SED_SIZE * sizeof(float));
+    float* sed = malloc(SED_SIZE * sizeof(float));
 
     return sed;
 }
-    
-void
-sed_copy(float *dst, float *src)
+
+void sed_copy(float* dst, float* src)
 {
     memcpy(dst, src, SED_SIZE * sizeof(float));
 }
 
-float *
-sed_dup(float *sed)
+float*
+sed_dup(float* sed)
 {
-    float *result = sed_alloc();
+    float* result = sed_alloc();
 
     sed_copy(result, sed);
     return result;
 }
 
-float
-sed_sum(float *sed)
+float sed_sum(float* sed)
 {
     float area = 0.0;
     int i;
 
     for (i = 0; i < SED_SIZE; i++)
-	area += sed[i];
+        area += sed[i];
 
     return area * SED_LAMBDA_STEP;
 }
 
-void
-sed_multiply_by(float *e, float *f)
+void sed_multiply_by(float* e, float* f)
 {
     for (int i = 0; i < SED_SIZE; i++)
-	e[i] = e[i] * f[i];
+        e[i] = e[i] * f[i];
 }
 
-float *
+float*
 sed_blackbody(int temperature)
 {
     assert(temperature >= 0.0);
-    float *result = sed_alloc();
+    float* result = sed_alloc();
 
+    double c1 = 3.7415e29;
+    double c2 = 1.4388e7;
 
-    double c1 = 3.74150E29;
-    double c2 = 1.4388E7;
-    
-SED_BLACKBODY_MISSINGCODE
+    double alpha = pow(560, 5) * (exp(c2 / (560 * temperature)) - 1) / c1;
+
+    int lambda;
+    for (int i = 0; i < SED_SIZE; i++) {
+        lambda = SED_LAMBDA_MIN + (i * SED_LAMBDA_STEP);
+        result[i] = alpha * c1 / (pow(lambda, 5) * (exp(c2 / (lambda * temperature)) - 1));
+    }
 
     return result;
 }
-
--- td2/xyz.c
+++ td2/xyz.c
@@ -1,60 +1,65 @@
-#include "sed.h"
-#include "colorimetric_data.h"
 #include "xyz.h"
+#include "colorimetric_data.h"
+#include "sed.h"
 
 #include "MISSINGCODE.h"
+
 /*
   sRGB transform (REC-709 with D65)
-
-  static float rec709_m_XYZ_to_sRGB[3][3] = 
-  {
-  {  3.240479, -1.537150, -0.498535 },
-  { -0.969256,  1.875992,  0.041556 },
-  {  0.055648, -0.204043,  1.057311 } 
-  };
-
 */
+static float rec709_m_XYZ_to_sRGB[3][3] = {
+    { 3.240479, -1.537150, -0.498535 },
+    { -0.969256, 1.875992, 0.041556 },
+    { 0.055648, -0.204043, 1.057311 }
+};
 
-float
-xyz_srgb_r(float cie_x, float cie_y, float cie_z)
+float xyz_srgb_r(float cie_x, float cie_y, float cie_z)
 {
-XYZ_SRGB_R_MISSINGCODE
+    return rec709_m_XYZ_to_sRGB[0][0] * cie_x + rec709_m_XYZ_to_sRGB[0][1] * cie_y + rec709_m_XYZ_to_sRGB[0][2] * cie_z;
 }
 
-float
-xyz_srgb_g(float cie_x, float cie_y, float cie_z)
+float xyz_srgb_g(float cie_x, float cie_y, float cie_z)
 {
-XYZ_SRGB_G_MISSINGCODE
+    return rec709_m_XYZ_to_sRGB[1][0] * cie_x + rec709_m_XYZ_to_sRGB[1][1] * cie_y + rec709_m_XYZ_to_sRGB[1][2] * cie_z;
 }
 
-float
-xyz_srgb_b(float cie_x, float cie_y, float cie_z)
+float xyz_srgb_b(float cie_x, float cie_y, float cie_z)
 {
-XYZ_SRGB_B_MISSINGCODE
+    return rec709_m_XYZ_to_sRGB[2][0] * cie_x + rec709_m_XYZ_to_sRGB[2][1] * cie_y + rec709_m_XYZ_to_sRGB[2][2] * cie_z;
 }
-
 
 /* 
    Computation of CIE-XYZ of DSE e with a normalization corresponding to a light
    source s. Store the result at the address given by xyz supposed to be that of
    an array of three floats.
 */
-void
-xyz_from_sed(float *xyz, float *e, float k)
+void xyz_from_sed(float* xyz, float* e, float k)
 {
-XYZ_FROM_SED_MISSINGCODE    
-}
+    float* ep = sed_dup(e);
+    sed_multiply_by(ep, cie_x_bar);
+    xyz[0] = k * sed_sum(ep);
 
+    sed_copy(ep, e);
+    sed_multiply_by(ep, cie_y_bar);
+    xyz[1] = k * sed_sum(ep);
+
+    sed_copy(ep, e);
+    sed_multiply_by(ep, cie_z_bar);
+    xyz[2] = k * sed_sum(ep);
+
+    free(ep);
+}
 
 /*
   Computation of the normalization coefficient associated with a light source s
 */
-
-float
-xyz_get_k_from_s(float *s)
+float xyz_get_k_from_s(float* s)
 {
-XYZ_GET_K_FROM_S_MISSINGCODE
-    
+    float* tmp = sed_dup(s);
+
+    sed_multiply_by(tmp, cie_y_bar);
+    float k = 1 / sed_sum(tmp);
+
+    free(tmp);
     return k;
 }
-    
