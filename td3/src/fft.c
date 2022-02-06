d61eadb34cb3b9baec5ad98251c436a2ff56b0b9
Bastien Soucasse <bastien.soucasse@icloud.com>
2022-02-06 19:46:49 +0100

    Activity 2 – TD 3

--- td3/src/fft.c
+++ td3/src/fft.c
@@ -13,7 +13,7 @@ conv_uchar_to_complex(unsigned char* channel, int width, int height)
     char sign;
     for (int y = 0; y < height; y++)
         for (int x = 0; x < width; x++) {
-            sign = (x + y) % 2 == 1 ? 1 : -1;
+            sign = (x + y) % 2 == 0 ? 1 : -1;
             c_channel[x * width + y] = sign * (double)channel[x * width + y] / (width * height);
         }
 
@@ -29,7 +29,7 @@ conv_complex_to_uchar(fftw_complex* c_channel, int width, int height)
     char sign;
     for (int y = 0; y < height; y++)
         for (int x = 0; x < width; x++) {
-            sign = (x + y) % 2 == 1 ? 1 : -1;
+            sign = (x + y) % 2 == 0 ? 1 : -1;
             channel[x * width + y] = sign * c_channel[x * width + y];
         }
 
@@ -60,5 +60,33 @@ fft_backward(fftw_complex* fr_chan, int width, int height)
     unsigned char* channel = conv_complex_to_uchar(out, width, height);
 
     fftw_destroy_plan(plan);
+    // fftw_free(out);
     return channel;
+}
+
+double*
+fft_extract_as(fftw_complex* fr_chan, int width, int height)
+{
+    double* as = (double*)malloc(width * height * sizeof(double));
+    for (unsigned int i = 0; i < width * height; i++)
+        as[i] = sqrt(pow(creal(fr_chan[i]), 2) + pow(cimag(fr_chan[i]), 2));
+    return as;
+}
+
+double*
+fft_extract_ps(fftw_complex* fr_chan, int width, int height)
+{
+    double* ps = (double*)malloc(width * height * sizeof(double));
+    for (int i = 0; i < width * height; i++)
+        ps[i] = atan2(cimag(fr_chan[i]), creal(fr_chan[i]));
+    return ps;
+}
+
+fftw_complex*
+fft_compose_aps(double* as, double* ps, int width, int height)
+{
+    fftw_complex* fr_chan = (fftw_complex*)fftw_malloc(width * height * sizeof(double));
+    for (int i = 0; i < width * height; i++)
+        fr_chan[i] = as[i] * cos(ps[i]) + as[i] * sin(ps[i]) * I;
+    return fr_chan;
 }
