336b18993664085c0a3d5f64c2e80c33c3d5dd15
Bastien Soucasse <bastien.soucasse@icloud.com>
2022-02-16 11:44:38 +0100

    ex2/3

--- td4/mmip.c
+++ td4/mmip.c
@@ -18,7 +18,8 @@ L_diff(unsigned char* i1, unsigned char* i2, int size)
 {
     unsigned char* result = malloc(size * sizeof(unsigned char));
 
-    L_DIFF_MISSINGCODE
+    for (int i = 0; i < size; i++)
+        result[i] = i1[i] - i2[i];
 
     return result;
 }
@@ -28,41 +29,67 @@ mmip_disk_erosion(unsigned char* channel, int width, int height, int radius)
 {
     unsigned char* result = malloc(width * height * sizeof(unsigned char));
 
-    MMIP_DISK_EROSION_MISSINGCODE
+    for (int ip = 0; ip < height; ip++)
+        for (int jp = 0; jp < width; jp++)
+            result[ip * width + jp] = strelm_disk_getmin(channel, width, height, ip, jp, radius);
+
+    return result;
 }
 
 unsigned char*
 mmip_disk_dilatation(unsigned char* channel, int width, int height, int radius)
 {
-    MMIP_DISK_DILATATION_MISSINGCODE
+    unsigned char* result = malloc(width * height * sizeof(unsigned char));
+
+    for (int ip = 0; ip < height; ip++)
+        for (int jp = 0; jp < width; jp++)
+            result[ip * width + jp] = strelm_disk_getmax(channel, width, height, ip, jp, radius);
+
+    return result;
 }
 
 unsigned char*
 mmip_disk_opening(unsigned char* channel, int width, int height, int radius)
 {
-    MMIP_DISK_OPENING_MISSINGCODE
+    unsigned char* e = mmip_disk_erosion(channel, width, height, radius);
+    unsigned char* d = mmip_disk_dilatation(e, width, height, radius);
+    free(e);
+    return d;
 }
 
 unsigned char*
 mmip_disk_closing(unsigned char* channel, int width, int height, int radius)
 {
-    MMIP_DISK_CLOSING_MISSINGCODE
+    unsigned char* d = mmip_disk_dilatation(channel, width, height, radius);
+    unsigned char* e = mmip_disk_erosion(d, width, height, radius);
+    free(d);
+    return e;
 }
 
 unsigned char*
 mmip_disk_bgrad(unsigned char* channel, int width, int height, int radius)
 {
-    MMIP_DISK_BGRAD_MISSINGCODE
+    unsigned char* d = mmip_disk_dilatation(channel, width, height, radius);
+    unsigned char* e = mmip_disk_erosion(channel, width, height, radius);
+    unsigned char* result = L_diff(d, e, width * height);
+    free(d), free(e);
+    return result;
 }
 
 unsigned char*
 mmip_disk_igrad(unsigned char* channel, int width, int height, int radius)
 {
-    MMIP_DISK_IGRAD_MISSINGCODE
+    unsigned char* e = mmip_disk_erosion(channel, width, height, radius);
+    unsigned char* result = L_diff(channel, e, width * height);
+    free(e);
+    return result;
 }
 
 unsigned char*
 mmip_disk_ograd(unsigned char* channel, int width, int height, int radius)
 {
-    MMIP_DISK_OGRAD_MISSINGCODE
+    unsigned char* d = mmip_disk_dilatation(channel, width, height, radius);
+    unsigned char* result = L_diff(d, channel, width * height);
+    free(d);
+    return result;
 }
